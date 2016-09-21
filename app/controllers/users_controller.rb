d23acde2cbfa6b993b4fc2e0c3738f33b0eb5c93
Your Name <arssonist@yahoo.com>
2016-09-21 16:43:17 -0400

    made new and create and views forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
-  def create
+  def create(user_params)
+    @user = User.new
+
+    if @user.save
+  # flahes need to  ne added in the layout too
+      redirect_to root_url
+      notice: "Account succesfully created"
+    else
+      notice: "Account not created"
+      render :new
+    end
+
   end
 
   def show
+  end
+end
+def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
