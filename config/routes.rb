5f727da4da85ddb44696e35833c164861536f59a
wes <weshenshall@gmail.com>
2016-09-22 22:00:56 -0400

    got sessions working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'users#new'
 
   resources :users do
@@ -6,6 +8,22 @@ Rails.application.routes.draw do
       resources :pledges
     end
   end
+
+
+
+
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  resources :sessions
+
+
+
+
+
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
